b3460c8a2fd23f22fe289b7b20350e11d009028d
Inho Kim <goodboyih96@gmail.com>
2018-03-18 06:35:05 -0700

    added command structure

--- main.h
+++ main.h
@@ -12,13 +12,20 @@ typedef enum {
 typedef enum { false, true } bool;
 
 typedef struct {
-	char* cmd_str;
+	char str[MAX_LEN];
+	char abb[MAX_LEN];
 	CMD_TYPE type;
 	CMD exec;
 	bool param;
 } COMMAND;
 
-
+COMMAND[] = {
+	{ "help", "h", shell, help, false }, { "dir", "d", shell, dir, false },
+	{ "quit", "q", shell, quit, false }, { "history", "hi", shell, hist, false },
+	{ "dump", "du", memory, dump, true }, { "edit", "e", memory, edit, true },
+	{ "fill", "f", memory, fill, true }, { "reset", "\0", memory, reset, false },
+	{ "opcode", "\0", opcode, op, true }, { "opcodelist", "\0", opcode, oplist, false}
+}
 
 char cmd[MAX_LEN];
 
