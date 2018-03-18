b2f9f7c36668be004754947c23b65dc7ffa04477
Inho Kim <goodboyih96@gmail.com>
2018-03-18 07:04:24 -0700

    HELP command finished

--- 20161577.c
+++ 20161577.c
@@ -1,27 +1,49 @@
 #include "main.h"
 
+COMMAND findCMD(char*);
+void helpCMD();
+
 int main() {
+	COMMAND cmdExec;
 	while(1) {
 		printf("sicsim> ");
 		fgets(cmd, MAX_LEN, stdin);
-		switch(cmd_type()) {
-			case shell:
-				puts("Shell Command");
-				break;
-			case memory:
-				puts("Memory Command");
-				break;
-			case opcode:
-				puts("Opcode Command");
+		cmd[strlen(cmd) - 1] = '\0';
+		cmdExec = findCMD(cmd);
+
+		switch(cmdExec.exec) {
+			case help:
+				helpCMD();
 				break;
 		}
+		/*
+		   switch(cmd_type()) {
+		   case shell:
+		   puts("Shell Command");
+		   break;
+		   case memory:
+		   puts("Memory Command");
+		   break;
+		   case opcode:
+		   puts("Opcode Command");
+		   break;
+		   }
+		   */
 	}
 	return 0;
 }
 
+COMMAND findCMD(char* str) {
+	int i;
+	for(i = 0; i < CMD_CNT - 1; i++)
+		if(!strcmp(str, cmdList[i].str) || !strcmp(str, cmdList[i].abb))
+			return cmdList[i];
+	return cmdList[CMD_CNT - 1];
+}
+
 CMD_TYPE cmd_type() {
 	CMD_TYPE tp;
-	
+
 	switch(cmd[0]) {
 		case 'h':
 		case 'q':
@@ -40,4 +62,17 @@ CMD_TYPE cmd_type() {
 	if(tp == shell && !strncmp(cmd, "du", 2))
 		tp = memory;
 	return tp;
+}
+
+void helpCMD() {
+	printf("h[elp]\n"
+			"d[ir]\n"
+			"q[uit]\n"
+			"hi[story]\n"
+			"du[mp] [start, end]\n"
+			"e[dit] address, value\n"
+			"f[ill] start, end, value\n"
+			"reset\n"
+			"opcode mnemonic\n"
+			"opcodelist\n");
 }
